36ff63e5189e2fcada6ed306295299ebe12814f9
leoidiaz <zaid.leonardo@gmail.com>
2020-04-29 12:50:38 -0700

    Add Anime Model

---
+++ AnimeBrowser/Controllers/Model Controller/AnimeController.swift
@@ -0,0 +1,9 @@
+//
+//  AnimeController.swift
+//  AnimeBrowser
+//
+//  Created by Leonardo Diaz on 4/29/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import Foundation
---
+++ AnimeBrowser/Model/Anime.swift
@@ -0,0 +1,25 @@
+//
+//  Anime.swift
+//  AnimeBrowser
+//
+//  Created by Leonardo Diaz on 4/29/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import Foundation
+
+struct AnimeTopLevelObject: Decodable {
+    let top: [Anime]
+}
+
+struct Anime: Decodable {
+    let title: String
+    let url: URL
+    let imageURL: URL
+    let startDate: String
+}
+
+enum CodingKeys: String, CodingKey {
+    case imageURL = "image_url"
+    case startDate = "start_date"
+}
